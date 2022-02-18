49f714b6ed5deaf86cc1a33931af56d2de0473a6
Olga Nazarenko <87029353+helganaz@users.noreply.github.com>
2022-02-18 20:40:37 +0200

    добавляет ContactListItem

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ContactListItem from "components/ContactListItem";
 import s from './ContactList.module.css'
 
 function ContactList({ contacts, onDeleteContact }) {
@@ -8,10 +9,12 @@ function ContactList({ contacts, onDeleteContact }) {
         <ul className={s.list}>
             {contacts.map(({ id, name, number }) => (
                 <li className={s.contact} key={id}>
-                    {name}: {number}
-                    <button className={s.button} onClick={() => onDeleteContact(id)}>
-                        Delete
-                    </button>
+                    <ContactListItem
+                    id={id}
+                    name={name}
+                    number={number}
+                    onDeleteContact={onDeleteContact}
+                    />
                 </li>
             ))}
         </ul>
